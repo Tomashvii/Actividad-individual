672ddccdd8eebbfa52f50ac276cf338e0eb9bf6d
Tomashvii <ortizptomas@gmail.com>
2026-04-24 14:04:02 +0000

    add capacitor android and build workflow

---
+++ clase27-marzo/android/app/src/main/java/com/picsum/gallery/MainActivity.java
@@ -0,0 +1,5 @@
+package com.picsum.gallery;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ clase27-marzo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.picsum.gallery',
+  appName: 'PicsumGallery',
+  webDir: 'dist'
+};
+
+export default config;
